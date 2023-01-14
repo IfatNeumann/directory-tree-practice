ef40f00beae209d3d64471d4dfd44da44f722040
Ifat Neumann <ineumann19@gmail.com>
2023-01-14 16:21:23 +0000

    first commit

---
+++ Game/check_answers.js
@@ -0,0 +1,62 @@
+function check_answer_yellow_earings(){
+    if (document.getElementById("answer-field").value == "colors/yellow.css"){
+        alert("You are correct! Continue to the next exercise!");
+    }
+    else{
+        alert("You are not correct this time. Could it be a typo? Please try again!")
+    }
+}
+
+function check_answer_jacket_dress(){
+    if (document.getElementById("answer-field").value == "../dress.html"){
+        alert("You are correct! Continue to the next exercise!");
+    }
+    else{
+        alert("You are not correct this time. Could it be a typo? Please try again!")
+    }
+}
+
+function check_answer_strips_onesie(){
+    if (document.getElementById("answer-field").value == "../patterns/strips.js"){
+        alert("You are correct! Continue to the next exercise!");
+    }
+    else{
+        alert("You are not correct this time. Could it be a typo? Please try again!")
+    }
+}
+
+function check_answer_shirt_pants(){
+    if (document.getElementById("answer-field").value == "../bottoms/pants.html"){
+        alert("You are correct! Continue to the next exercise!");
+    }
+    else{
+        alert("You are not correct this time. Could it be a typo? Please try again!")
+    }
+}
+
+function check_answer_red_skirt(){
+    if (document.getElementById("answer-field").value == "../../colors/red.css"){
+        alert("You are correct! Continue to the next exercise!");
+    }
+    else{
+        alert("You are not correct this time. Could it be a typo? Please try again!")
+    }
+}
+
+function check_answer_black_pants(){
+    if (document.getElementById("color-answer-field").value == "../../colors/moreColors/black.css"){
+        alert("You are correct! Continue to the next exercise!");
+    }
+    else{
+        alert("You are not correct this time. Could it be a typo? Please try again!")
+    }
+}
+
+function check_answer_glitter_pants(){
+    if (document.getElementById("answer-field").value == "../../patterns/glitter.js"){
+        alert("You are correct! Continue to the next exercise!");
+    }
+    else{
+        alert("You are not correct this time. Could it be a typo? Please try again!")
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Directory Tree Practice
+
+I've created this game for the girls I teach in [Alicecode](https://alicecode.org/en) to help them practice the topic of paths of files, and how to connect between files, in an enjoyable way!
+
+## Getting Started
+
+Link to the game can be found [here](https://directory-tree-practice.netlify.app/)!
+
+The code is divided into two directories: `Game` and `Project`.
+`Game` is divided into three directories:
+- `BackgroundImages` - images visualizing the game excercises
+- `CodeImages` - screenshots of code snippets of the excercises and the directory tree the game is based on
+- `ExerciseHtmls` - the html files of the game itself
+
+Under `Project` you can find all the files, as they appear in the `CodeImages` screenshots.
+
+### You can contribute!
+
+I still need to figure out the whole "open source in git" concept, but this game has not yet reached it's full potential!
+Whether you want to practice html, css and js, or just passioate about this game as I am - you can help me make it better!
+
+#### A few improvement ideas:
+- A nicer text box for answers - maybe even on the image itself (with "coding font" like [Izayoi Monospaced](https://www.fontspace.com/izayoi-monospaced-font-f32299))
+- A better sidebars: the current one overrides the page + the sidebar is hard coded in each page
+- An automatic transfer to the next page when the answer was correct
+- A start button at the buttom of the main page, leading to the first excercise
+
+Have more ideas? I would love to hear them! Please reach out via [LinkedIn](https://www.linkedin.com/in/ifatneumann/)!
